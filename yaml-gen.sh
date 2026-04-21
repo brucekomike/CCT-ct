0af3a4f160521396c56a0755812ec39f88af6beb
brucekomike <brucekomike@gmail.com>
2026-04-21 16:13:39 +0800

    add yaml generator

---
+++ yaml-gen.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# docker compose file generator
+# usage: $0 <servers> <port_start> <image_name> <filename>
+if [ "$1" = "" ]; then
+    echo "Usage: $0 <servers> <port_start> [image_name] [filename]"
+    exit 1
+fi
+servers=$1
+port_start=$2
+image_name=${3-"ghcr.io/brucekomike/cct-ct:latest"}
+filename=${4-"docker-compose.yaml"}
+
+echo "services:" > $filename
+for i in $(seq 1 $servers); do
+    port=$((port_start + i - 1))
+    echo "  env$i:" >> $filename
+    echo "    image: $image_name" >> $filename
+    echo "    ports:" >> $filename
+    echo "      - \"8080:$port\"" >> $filename
+    echo "    environment:" >> $filename
+    echo "      - PASSWORD=code$port" >> $filename
+done
